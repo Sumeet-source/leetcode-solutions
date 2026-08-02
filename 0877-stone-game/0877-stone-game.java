4df706d7ba9157dc9dc88e11b056f0ff5cfafe2e
Sumeet Dhakad <74111734+Sumeet-source@users.noreply.github.com>
2026-08-02 11:40:10 +0530

    Time: 0 ms (100%), Space: 42.9 MB (76.69%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        // Alice can always force a win by picking all piles at even indices
+        // or all at odd indices. Since the total sum is odd, one group is larger.
+        return true;
+    }
+}
